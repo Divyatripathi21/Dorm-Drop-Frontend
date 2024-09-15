2310d82b242cbf9d2af408d35d352120a7bbac47
Divya prakash tripathi <divytripathidivy@gmail.com>
2024-09-15 17:11:39 +0530

    c12

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dorm-drop-backend-e975.vercel.app/',       //https://dormdrop.onrender.com   //http://localhost:3000
-        secure: false,
-      },
-    },
-  },
-  plugins: [react()],
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://dorm-drop-backend-e975.vercel.app/',       //https://dormdrop.onrender.com   //http://localhost:3000
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
